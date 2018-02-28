f6359a71844f76b0845d93e2d57b4143a7d3292e
karthibullet <35765123+karthibullet@users.noreply.github.com>
2018-02-28 16:21:47 +0530

    77.c

---
+++ 77.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+ int num,i,count=0;
+ printf("Enter the number");
+ scanf("%d",&num);
+ for(i=1;i<=num;i++)
+ {
+ if(num%i==0)
+ {
+ printf("%d\n",i);
+ }
+ }
+ }
